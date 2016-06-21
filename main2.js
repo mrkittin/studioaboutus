baccb113b605f15195b6c5eda843d3cf9672562c
Sergii Dumyk <zergaka@gmail.com>
2016-06-20 23:51:56 -0700

    Update main2.js

--- main2.js
+++ main2.js
@@ -6,3 +6,22 @@ var updateFcts	= [];
 var scene	= new THREE.Scene();
 var camera	= new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.01, 100 );
 camera.position.z = 3;
+
+//////////////////////////////////////////////////////////////////////////////////
+//		add an object and make it move					//
+//////////////////////////////////////////////////////////////////////////////////
+	
+window.AudioContext	= window.AudioContext || window.webkitAudioContext;
+var context	= new AudioContext();
+// create the videoTexture
+var videoTexture= new THREEx.VideoTexture('videos/sintel.ogv');
+updateFcts.push(function(delta, now){
+	videoTexture.update(delta, now);
+})
+// wait until the vid is loaded
+videoTexture.video.addEventListener('canplaythrough', function(event){
+	// create a source node from the mediaElement
+	var sourceNode	= context.createMediaElementSource(videoTexture.video);
+	// connect it to WebAudio API
+	sourceNode.connect(context.destination);
+})
